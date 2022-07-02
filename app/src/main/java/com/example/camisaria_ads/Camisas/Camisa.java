19eea448e5e7cf65ae95a2c6989aac98a314146b
MarceloAAmaral <marceloamaralfpolis@gmail.com>
2022-07-02 10:46:11 -0300

    CM - Inicio do projeto

---
+++ app/src/main/java/com/example/camisaria_ads/Camisas/Camisa.java
@@ -0,0 +1,42 @@
+package com.example.camisaria_ads.Camisas;
+
+public class Camisa {
+ /* encapsulamento */
+    private int id;
+    private String tipo;
+    private String tamanho;
+    private String cor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+}
